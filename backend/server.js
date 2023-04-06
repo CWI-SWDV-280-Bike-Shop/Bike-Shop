797257866a39aa852e5c0714488cb1317ec1e4d0
Nicko Goodwin <33714480+nickogoodwin@users.noreply.github.com>
2023-04-06 08:42:02 -0600

    moved server.js to root and removed unneeded packages

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,11 @@
-// Patches
-import error from 'express-custom-error';
-const { inject, errorHandler } = error;
-inject(); // Patch express in order to use async / await syntax
-
 // Import Dependencies
+import dotenv from 'dotenv';
 import express from 'express';
-import cookieParser from 'cookie-parser';
 import cors from 'cors';
-import helmet from 'helmet';
-import mandatoryenv from 'mandatoryenv';
-import router from './routes/router.js';
+import router from './src/routes/router.js';
+dotenv.config();
 
-// Load .env Enviroment Variables to process.env
-mandatoryenv.load(['DB_URL', 'PORT', 'SECRET']);
-
-const { PORT } = process.env;
+const PORT = process.env.port || 3000;
 
 // Instantiate an Express Application
 const app = express();
@@ -22,10 +13,7 @@ const app = express();
 // Configure Express App Instance
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
-
-app.use(cookieParser());
 app.use(cors());
-app.use(helmet());
 
 // This middleware adds the json header to every response
 app.use('*', (req, res, next) => {
@@ -36,9 +24,6 @@ app.use('*', (req, res, next) => {
 // Assign Routes
 app.use('/api', router);
 
-// Handle errors
-app.use(errorHandler());
-
 // Handle not valid route
 app.use('*', (req, res) => {
   res.status(404).json({ status: false, message: 'Endpoint Not Found' });
@@ -46,4 +31,3 @@ app.use('*', (req, res) => {
 
 // Open Server on selected Port
 app.listen(PORT, () => console.info('Server listening on port ', PORT));
-

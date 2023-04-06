daba6fc21a509e88b71555579d867e039e503ba1
Solomon Victorino <git@solomonvictorino.com>
2023-04-06 13:28:13 -0600

    capitalize ATLAS_PASS

--- backend/src/config/database.config.js
+++ backend/src/config/database.config.js
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const db = {
-  MongoDBAtlas: `mongodb+srv://${process.env.ATLAS_USERNAME}:${process.env.ATLAS_pass}@bikeshop.v1dhfuf.mongodb.net/BikeShop?retryWrites=true&w=majority`,
+  MongoDBAtlas: `mongodb+srv://${process.env.ATLAS_USERNAME}:${process.env.ATLAS_PASS}@bikeshop.v1dhfuf.mongodb.net/BikeShop?retryWrites=true&w=majority`,
 };
 
 export default db;

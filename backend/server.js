dbef8b971e1dce779e86b134677b15b9230d4593
Nicko Goodwin (Home) <33714480+nickogoodwin@users.noreply.github.com>
2023-04-06 13:24:34 -0600

    capitalized PORT

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ import mongoose from 'mongoose';
 import db from './src/config/database.config.js';
 dotenv.config();
 
-const PORT = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
 
 // Instantiate an Express Application
 const app = express();

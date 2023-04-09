f7121749296adff0884d61a526a069901a387122
Nicko Goodwin <33714480+nickogoodwin@users.noreply.github.com>
2023-04-09 14:48:33 -0600

    finished order controller. we now have bike, accessory, and service documents populating orders  based on their respective models, and filtering based on the contents of the order (the 'items' within)

--- backend/src/controllers/order.controller.js
+++ backend/src/controllers/order.controller.js
@@ -3,17 +3,54 @@ import { NotFound } from '../errors.js';
 
 const OrderController = {
   async getOrders(req, res) {
-    let { customer, itemId, itemType } = req.query;
+    let {
+      customer,
+      itemId,
+      itemType,
+      itemCategory,
+      bikeColor,
+      bikeSize,
+      bikeGender,
+      serviceDate,
+    } = req.query;
 
-    const orders = await Order.find({
-      'customer': customer,
-      'items.item': itemId,
-      'items.itemModel': itemType,
-    })
-      .populate('customer')
-      .populate('items.item');
+    let query = {};
 
-    res.status(200).json(orders);
+    if (customer) query.customer = customer;
+    if (itemId) query['items.item'] = itemId;
+    if (itemType) query['items.itemModel'] = itemType;
+
+    // Example of the query if all params are present
+    // query = {
+    //   'customer': customer,
+    //   'items.item': itemId,
+    //   'items.itemModel': itemType,
+    // };
+
+    const orders = await Order.find(query).populate('customer').populate({
+      path: 'items.item',
+      model: itemType,
+    });
+
+    // After retrieving the orders, filter them based on the contents of their "items"
+    let filteredOrders = orders;
+
+    // this is a little ugly and could probably be cleaned up, but it provides the functionality
+    // that i think we're looking for
+    if (itemCategory || bikeColor || bikeSize || bikeGender || serviceDate) {
+      filteredOrders = orders.filter((order) => {
+        return order.items.some((item) => {
+          return (
+            (!itemCategory || item.item.category === itemCategory) &&
+            (!bikeColor || item.item.color === bikeColor) &&
+            (!bikeSize || item.item.size === bikeSize) &&
+            (!bikeGender || item.item.gender === bikeGender)
+          );
+        });
+      });
+    }
+
+    res.status(200).json(filteredOrders);
   },
 
   async getOrderById(req, res) {
--- backend/src/models/order.model.js
+++ backend/src/models/order.model.js
@@ -1,28 +1,28 @@
 import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
+const ItemSchema = new Schema({
+  item: {
+    type: Schema.Types.ObjectId,
+    required: true,
+    refPath: 'items.itemModel',
+  },
+  itemModel: {
+    type: String,
+    required: true,
+    enum: ['Bike', 'Accessory', 'Service'],
+  },
+  price: Number,
+  quantity: Number,
+});
+
 const OrderSchema = new Schema(
   {
     customer: {
       type: Schema.Types.ObjectId,
       ref: 'User',
     },
-    items: [
-      {
-        item: {
-          type: Schema.Types.ObjectId,
-          required: true,
-          refPath: 'items.itemModel',
-        },
-        itemModel: {
-          type: String,
-          required: true,
-          enum: ['Bike', 'Accessory', 'Service'],
-        },
-        price: Number,
-        quantity: Number,
-      },
-    ],
+    items: [ItemSchema],
     total: Number,
   },
   { timestamps: true }

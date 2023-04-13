5d8615f7854e80330c7b7b3f91a9e2c956dfb3e0
JoelWeers <42760707+JoelWeers@users.noreply.github.com>
2023-04-13 13:49:25 -0600

    Added a code for the navbar to respond to web vs android

--- frontend/src/components/Navigation/Header.tsx
+++ frontend/src/components/Navigation/Header.tsx
@@ -1,55 +1,99 @@
-import { DrawerHeaderProps } from '@react-navigation/drawer';
-import React from 'react';
-import { TouchableOpacity, Text, Image, View, StyleSheet } from 'react-native';
-import Icon from 'react-native-vector-icons/Ionicons';
+import { DrawerHeaderProps } from "@react-navigation/drawer";
+import React from "react";
+import { TouchableOpacity, Text, Image, View, StyleSheet, Platform } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons'
 
-//Navigation Header
-export const NavigationHeader = ({ navigation }: DrawerHeaderProps) => {
-  return (
-    <View style={styles.headerView}>
+const NavigationBar = ({navigation}: DrawerHeaderProps) => {
+
+  if (Platform.OS === 'android' || Platform.OS === 'ios') {
+    return (
       <View style={styles.headerIcons}>
-        <TouchableOpacity
-          style={styles.headerTouchable}
-          onPress={navigation.openDrawer}
-        >
-          <Icon name="menu-outline" size={40} color="#FFF" />
+        <TouchableOpacity style={styles.headerTouchable} onPress={navigation.openDrawer}>
+          <Icon
+            name="menu-outline"
+            size={40}
+            color="#FFF"
+          />
         </TouchableOpacity>
       </View>
+    );
+  } else if (Platform.OS === 'web') {
+    return (
+      <View style={styles.navBar}>
+        <TouchableOpacity onPress={() => navigation.navigate("Home")}>
+          <Text style={styles.navText}>Home</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={() => navigation.navigate("Shop")}>
+          <Text style={styles.navText}>Shop</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={() => navigation.navigate("Login")}>
+          <Text style={styles.navText}>Login</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={() => navigation.navigate("Profile")}>
+          <Text style={styles.navText}>Profile</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={() => navigation.navigate("ReduxTestPage")}>
+          <Text style={styles.navText}>Redux Test Page</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+
+}
+
+//Navigation Header
+export const NavigationHeader = (props: DrawerHeaderProps) => {
+  return (
+    <View style={styles.headerView} >
+      <NavigationBar {...props} />
       <View style={styles.headerLogoParent}>
-        <Image
-          source={require('@assets/Branding/OfficialLogo-white.png')}
-          style={styles.headerLogo}
-        />
+        <Image source={require('../../assets/Media/Branding/OfficialLogo-white.png')} style={styles.headerLogo} />
       </View>
       <View style={styles.headerIcons}>
         <TouchableOpacity style={styles.headerTouchable}>
-          <Icon name="cart-outline" size={40} color="#FFF" />
+          <Icon name="cart-outline"
+            size={40}
+            color="#FFF"
+          />
         </TouchableOpacity>
       </View>
     </View>
   );
-};
+}
 //Header Stylesheetr
 const styles = StyleSheet.create({
   headerView: {
-    flexDirection: 'row',
-    backgroundColor: '#6A7B76',
-    paddingTop: 7,
+    flexDirection: "row", backgroundColor: "#6A7B76", paddingTop: 7, justifyContent: "center",
   },
   headerIcons: {
-    flexDirection: 'column',
-    justifyContent: 'center',
+    flexDirection: "column", justifyContent: "center"
   },
   headerLogoParent: {
-    flexDirection: 'row',
-    flex: 1,
-    justifyContent: 'center',
+    flexDirection: "row", flex: 1, justifyContent: 'center'
   },
   headerLogo: {
-    width: 180,
-    height: 120,
+    width: 180, height: 120,
   },
   headerTouchable: {
-    margin: 8,
+    margin: 8
   },
-});
+  navBar: {
+    // flex: 1,
+    flexDirection: "row",
+    alignItems: "center",
+    //position: "absolute",
+  },
+  navText: {
+    color: '#FFF',
+    fontSize: 25,
+    margin: 8,
+    borderColor: "#FFF",
+    borderWidth: 2,
+    borderRadius: 10,
+    padding: 5,
+  }
+})

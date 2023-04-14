f62c6c757e4572c60c70d37901ad0deabdd67e99
Nicko Goodwin <33714480+nickogoodwin@users.noreply.github.com>
2023-04-14 02:45:30 -0600

    renamed Components/Context to Components/ContextAPI

--- frontend/pages/reduxTestPage.tsx
+++ frontend/pages/reduxTestPage.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { ScrollView, Text } from 'react-native';
 import Styles from '../Styles';
 import { Auth, Bikes } from '../Components/Redux';
-import { AuthSection } from '../Components/Context';
+import { AuthSection } from '../Components/ContextAPI';
 
 export const ReduxTestPage = () => {
   return (

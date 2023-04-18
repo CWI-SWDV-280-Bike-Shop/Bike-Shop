4469ce30dbfde5e637d70f67c1f898dcaed1af29
Solomon Victorino <git@solomonvictorino.com>
2023-04-18 11:06:56 -0600

    finish integration of base router with controllers

--- backend/src/routes/base.controller.js
+++ backend/src/routes/base.controller.js
@@ -2,38 +2,35 @@ import { NotFound } from '../errors/errors.js';
 
 export const BaseController = (mongooseModel) =>
   class BaseController {
-    static async find(req, res) {
-      const results = await mongooseModel.find(req.query);
-      res.status(200).json(results);
+    static find(query = {}) {
+      return mongooseModel.find(query);
     }
 
-    static async getById(req, res) {
-      const result = await mongooseModel.findById(req.params.id);
+    static async getById({ id } = {}) {
+      const result = await mongooseModel.findById(id);
       if (!result) throw new NotFound();
-      res.status(200).json(result);
+      return result;
     }
 
-    static async create(req, res) {
-      const data = new mongooseModel(req.body);
-      const newData = await data.save();
-      res.status(200).json(newData);
+    static create(data) {
+      return new mongooseModel(data).save();
     }
 
-    static async update(req, res) {
+    static async update(data) {
       const updated = await mongooseModel.findByIdAndUpdate(
-        req.params.id,
-        req.body,
+        data.id,
+        data,
         {
           new: true,
         }
       );
       if (!updated) throw new NotFound();
-      res.status(200).json(updated);
+      return updated;
     }
 
-    static async delete(req, res) {
-      const deleted = await mongooseModel.findByIdAndDelete(req.params.id);
+    static async delete({ id }) {
+      const deleted = await mongooseModel.findByIdAndDelete({ id });
       if (!deleted) throw new NotFound();
-      res.status(200).json({ message: 'Deleted successfully' });
+      return { message: 'Deleted successfully' };
     }
   };

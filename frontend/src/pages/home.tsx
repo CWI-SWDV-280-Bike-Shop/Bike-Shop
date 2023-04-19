830a6e00dc4335afae73b382ef897ec2e303d433
William Beukelman <williambeukelman@my.cwi.edu>
2023-04-19 01:39:38 -0600

    Updated home to have more web friendly styling and seperation, also added icons to build

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import Icon from 'react-native-vector-icons/Ionicons';
-import { Text, StyleSheet, View, ScrollView } from 'react-native';
+import { Text, StyleSheet, View, ScrollView, Platform } from 'react-native';
 import Swiper from 'react-native-swiper/src';
 
 // Slide component imports
@@ -9,84 +9,119 @@ import { Slide } from '@components/Slide';
 //Footer Import
 import { Footer } from '@components/Footer';
 
+const CustomSwiper = () => {
+  return (
+    <Swiper>
+      <Slide
+        name={'City Bikes'}
+        description={
+          'Shop our collection of bikes built for busy city streets.'
+        }
+        imgSrc={require('@assets/Images/citybikestockimg.png')}
+        btnName={'Shop'}
+      />
+      <Slide
+        name={'eBikes'}
+        description={
+          'Our eBikes are efficient and will save you money on your commute.'
+        }
+        imgSrc={require('@assets/Images/ebikestockimg.png')}
+        btnName={'Shop'}
+      />
+      <Slide
+        name={"Kid's Bikes"}
+        description={'Bikes for anyone in your family, available now.'}
+        imgSrc={require('@assets/Images/kidsbikestockimg.png')}
+        btnName={'Shop'}
+      />
+      <Slide
+        name={'Mountain Bikes'}
+        description={
+          'We have rugged mountain bikes to get you out on the trails.'
+        }
+        imgSrc={require('@assets/Images/mtnbikestockimg.png')}
+        btnName={'Shop'}
+      />
+      <Slide
+        name={'Repair Shop'}
+        description={
+          'If your bike breaks down, our repair shop has you covered. Give us a call!'
+        }
+        imgSrc={require('@assets/Images/repairshopimg.png')}
+        btnName={'Contact'}
+      />
+    </Swiper>
+  );
+}
+
+const quote = `"We aren't just a bike shop, we're a community."`;
+const mission = [`Our mission at Wheely Good Bikes is to provide high-quality bicycles and accessories that promote a healthy and sustainable lifestyle. We are committed to offering personalized service and expert advice to help our customers choose the right bike for their needs and skill level.`, 
+                `Our goal is to create a welcoming environment where cyclists of all ages and abilities can come together to share their passion for cycling and enjoy the freedom of the open road.`, 
+                `At the heart of our mission is a dedication to promoting eco-friendly transportation options and helping our community reduce its carbon footprint.`]
+
 export const Home = () => {
   const slideNames = ['City Bikes'];
-  return (
-    <View style={[styles.container]}>
+  //const onlyMobile = (code) => { if ((Platform.OS === 'android' || Platform.OS === 'ios')){ return code } }
+  //const onlyWeb = (code) => { if (Platform.OS === 'web'){ return code } }
+  if ((Platform.OS === 'android' || Platform.OS === 'ios')) {
+    return (
+      <View style={[styles.container]}>
+        <View style={[styles.slidesContainer]}>
+          <CustomSwiper/>
+        </View>
+        <View style={[styles.contentContainer]}>
+            <ScrollView>
+            {/* <View style={[styles.titleRow]}>
+              <Text style={[styles.title]}>Wheely Good Bikes</Text>
+            </View> */}
+            <View style={[styles.quoteRow]}>
+              <Text style={[styles.quote]}>
+                {quote}
+              </Text>
+            </View>
+            <View style={[styles.missionStatementRow]}>
+              <Text style={[styles.missionStatement]}>
+                {mission}
+              </Text>
+            </View>
+            <Footer />
+            </ScrollView>
+        </View>
+      </View>
+    );
+  } else {
+    return (
+    <ScrollView>
+      <View style={[styles.container]}>
       <View style={[styles.slidesContainer]}>
-        <Swiper>
-          <Slide
-            name={'City Bikes'}
-            description={
-              'Shop our collection of bikes built for busy city streets.'
-            }
-            imgSrc={require('@assets/Images/citybikestockimg.png')}
-            btnName={'Shop'}
-          />
-          <Slide
-            name={'eBikes'}
-            description={
-              'Our eBikes are efficient and will save you money on your commute.'
-            }
-            imgSrc={require('@assets/Images/ebikestockimg.png')}
-            btnName={'Shop'}
-          />
-          <Slide
-            name={"Kid's Bikes"}
-            description={'Bikes for anyone in your family, available now.'}
-            imgSrc={require('@assets/Images/kidsbikestockimg.png')}
-            btnName={'Shop'}
-          />
-          <Slide
-            name={'Mountain Bikes'}
-            description={
-              'We have rugged mountain bikes to get you out on the trails.'
-            }
-            imgSrc={require('@assets/Images/mtnbikestockimg.png')}
-            btnName={'Shop'}
-          />
-          <Slide
-            name={'Repair Shop'}
-            description={
-              'If your bike breaks down, our repair shop has you covered. Give us a call!'
-            }
-            imgSrc={require('@assets/Images/repairshopimg.png')}
-            btnName={'Contact'}
-          />
-        </Swiper>
+        <CustomSwiper/>
       </View>
       <View style={[styles.contentContainer]}>
-        <ScrollView>
-          {/* <View style={[styles.titleRow]}>
-            <Text style={[styles.title]}>Wheely Good Bikes</Text>
-          </View> */}
           <View style={[styles.quoteRow]}>
             <Text style={[styles.quote]}>
-              "We aren't just a bike shop, we're a community."
+              {quote}
             </Text>
           </View>
           <View style={[styles.missionStatementRow]}>
             <Text style={[styles.missionStatement]}>
-              Our mission at Wheely Good Bikes is to provide high-quality
-              bicycles and accessories that promote a healthy and sustainable
-              lifestyle. We are committed to offering personalized service and
-              expert advice to help our customers choose the right bike for
-              their needs and skill level. Our goal is to create a welcoming
-              environment where cyclists of all ages and abilities can come
-              together to share their passion for cycling and enjoy the freedom
-              of the open road. At the heart of our mission is a dedication to
-              promoting eco-friendly transportation options and helping our
-              community reduce its carbon footprint.
+              {mission[0]}
+            </Text>
+            <Text style={[styles.missionStatement]}>
+              {mission[1]}
+            </Text>
+            <Text style={[styles.missionStatement]}>
+              {mission[2]}
             </Text>
           </View>
           <Footer />
-        </ScrollView>
       </View>
     </View>
-  );
+  </ScrollView>
+    );
+  }
 };
 
-const styles = StyleSheet.create({
+const mobile = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: 'column',
@@ -133,7 +168,7 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     flex: 1,
     marginTop: 20,
-    marginBottom: 20,
+    marginBottom: 20
   },
   missionStatement: {
     alignSelf: 'center',
@@ -144,3 +179,62 @@ const styles = StyleSheet.create({
     paddingRight: 15,
   },
 });
+
+//Web Stylesheet
+const web = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+  },
+  slidesContainer: {
+    flexDirection: 'row',
+    flex: 3,
+  },
+  contentContainer: {
+    flexDirection: 'column',
+    flex: 2,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingHorizontal: '10%'
+  },
+  quoteRow: {
+    margin: 10,
+    paddingHorizontal: 30,
+    paddingVertical: 20,
+    borderRadius: 5,
+    backgroundColor: '#446058',
+    width: 'fit-content'
+  },
+  quote: {
+    fontSize: 27,
+    color: 'white',
+    textShadowColor: 'rgba(0, 0, 0, 0.85)',
+    textShadowOffset: { width: -1, height: 1 },
+    textShadowRadius: 10,
+    fontStyle: 'italic',
+    textAlign: 'center',
+  },
+  missionStatementRow: {
+    flexDirection: 'row',
+    //flex: 1, 
+    marginTop: 20,
+    marginBottom: 20,
+  },
+  missionStatement: {
+    borderLeftWidth: 5,
+    borderColor: '#03312E',
+    alignSelf: 'flex-start',
+    textAlign: 'left',
+    fontSize: 20,
+    color: '#03312E',
+    paddingLeft: 15,
+    paddingRight: 15
+  },
+});
+
+let styles;
+if ((Platform.OS === 'android' || Platform.OS === 'ios')) {
+  styles = mobile;
+} else {
+  styles = web;
+}

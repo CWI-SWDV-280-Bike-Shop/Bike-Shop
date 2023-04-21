a7a54222fe56f97ae0fdcde423370867779a26e5
Solomon Victorino <git@solomonvictorino.com>
2023-04-20 19:10:30 -0600

    improve order search, always populate customer/items

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,7 @@ dotenv.config();
 const app = express();
 
 // Configure Express App Instance
+app.set('query parser', 'extended')
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(cors());
--- backend/src/routes/orders/order.controller.js
+++ backend/src/routes/orders/order.controller.js
@@ -1,6 +1,25 @@
 import Order from './order.model.js';
 import { BaseController } from '../base.controller.js';
 
-class OrderController extends BaseController(Order) {}
+class OrderController extends BaseController(Order) {
+  static find(query = {}) {
+    return Order.find({
+      ...query,
+      items: {
+        $elemMatch: query.items ?? {}
+      }
+    })
+      .populate('customer')
+      .populate('items')
+      .populate('items.product');
+  }
+
+  static getById(query = {}) {
+    return Order.findById(query)
+      .populate('customer')
+      .populate('items')
+      .populate('items.product');
+  }
+}
 
 export default OrderController;

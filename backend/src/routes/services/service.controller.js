a9e3a7f6cd197e46d58004d450e7dc09adc49d3e
Solomon Victorino <git@solomonvictorino.com>
2023-04-20 19:18:39 -0600

    delete legacy bikes/accessories/services code

--- backend/src/routes/services/service.controller.js
+++
@@ -1,6 +0,0 @@
-import Service from '../../routes/services/service.model.js';
-import { BaseController } from '../base.controller.js';
-
-class ServiceController extends BaseController(Service) {}
-
-export default ServiceController;
--- backend/src/routes/services/service.route.js
+++
@@ -1,4 +0,0 @@
-import ServiceController from './service.controller.js';
-import { baseCRUD } from '../base.route.js';
-
-export default baseCRUD(ServiceController);

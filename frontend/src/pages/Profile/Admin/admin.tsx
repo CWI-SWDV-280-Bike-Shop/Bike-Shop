43ee92cafcd2005e8d8f01112e2d281bf22f0b52
William Beukelman <williambeukelman@my.cwi.edu>
2023-04-21 23:34:06 -0600

    Built out and styled basic table to hold user data

--- frontend/src/pages/Profile/Admin/admin.tsx
+++ frontend/src/pages/Profile/Admin/admin.tsx
@@ -1,5 +1,57 @@
-import * as React from 'react';
-import {  Text, StyleSheet, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import {  Text, StyleSheet, View, FlatList } from 'react-native';
+import Layout from '@styles/layout/Layout';
+import UserAPI from '@api/user.api';
+import { User } from '@/types/data.types';
+import { Product } from '@/types/data.types';
+import ProductAPI from '@/api/product.api';
+import { formatPrice } from '@/utilities/formatter';
+
+
+const TableHeader = ({labels} : {labels: string[]}) => {
+    return (
+      <View style={[styles.row, styles.rowHeader]}>
+        { labels.map((label) => (<View style={styles.col}><Text style={styles.rowHeaderText}>{label}</Text></View>)) }
+    </View>
+    )
+}
+
+const UserElement = ({user}: {user: User}) => (
+  <View style={styles.row}>
+    <View style={styles.col}><Text>{user?._id}</Text></View>
+    <View style={styles.col}><Text>{user?.name}</Text></View>
+    <View style={styles.col}><Text>{user?.email}</Text></View>
+    <View style={styles.col}><Text>{user?.role}</Text></View>
+    <View style={styles.col}><Text>{user?.phone}</Text></View>
+    <View style={styles.col}><Text>0</Text></View>
+    <View style={styles.col}><Text>{user?.address?.street}</Text></View>
+    <View style={styles.col}><Text>{user?.address?.city}</Text></View>
+    <View style={styles.col}><Text>{user?.address?.state}</Text></View>
+    <View style={styles.col}><Text>{user?.address?.zip}</Text></View>
+    <View style={styles.col}><Text>{user?.address?.country}</Text></View>
+  </View>
+);
+
+const ListUsers = () => {
+  const [users, setUser] = useState([{}] as [User]);
+
+  useEffect(() => {
+    UserAPI.getAll().then((res) => setUser(res.data));
+  }, []);
+
+  return (
+    <View style={styles.section}>
+      { users &&
+      <FlatList
+        data={users}
+        renderItem={({item}) => <UserElement user={item} />}
+        keyExtractor={(user: User) => user?._id}
+        ListHeaderComponent={() => <TableHeader labels={["id", "name", "email", "role", "phone", "orders", "street", "city", "state", "zip", "country"]}/>}
+      />
+      }
+    </View>
+  );
+};
 
 export const Admin = () => {
     return (
@@ -9,18 +61,53 @@ export const Admin = () => {
           <Text style={[styles.bodyText]}>
             This is the admin page.
           </Text>
+          <ListUsers />
         </View>
       </View>
     );
 };
 
 const styles = StyleSheet.create({
+  rowHeader: {
+    borderBottomColor: '#f0f1f1',
+    borderBottomWidth: 3,
+    textTransform: 'uppercase'
+  },
+  rowHeaderText: {
+    fontWeight: '700'
+  },
+  row: {
+    display: 'flex',
+    flexDirection: 'row',
+    borderBottomColor: '#f8f8f8',
+    borderBottomWidth: 1,
+  },
+  col: {
+    flex: 1,
+    paddingHorizontal: '.5rem',
+    paddingVertical: '.25rem'
+  },
+  section: {
+    borderTopColor: '#6a7b76',
+    borderTopWidth: 10,
+    margin: '2rem',
+    padding: '1rem',
+    borderRadius: 10,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.22,
+    shadowRadius: 2.22,
+    elevation: 1
+  },
   container: {
     flex: 1
   },
   contentContainer: {
     flex: 1,
-    backgroundColor: '#D3D5D4',
+    backgroundColor: '#fff',
   }, 
   header: {
     margin: 20,

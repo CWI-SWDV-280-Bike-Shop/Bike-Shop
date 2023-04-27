a7c440055882a271190cbc7e31c706e9b58a8c4e
William Beukelman <williambeukelman@my.cwi.edu>
2023-04-27 12:10:19 -0600

    Added env check to switch to local db

--- backend/src/config/database.config.js
+++ backend/src/config/database.config.js
@@ -1,8 +1,11 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
+const isLocal = process.env.hasOwnProperty('MONGO_USER');
+
 const db = {
-  MongoDBAtlas: `mongodb+srv://${process.env.ATLAS_USERNAME}:${process.env.ATLAS_PASS}@bikeshop.v1dhfuf.mongodb.net/BikeShop?retryWrites=true&w=majority`,
+  MongoDB: (isLocal) ? `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@bikeshop-mongo:27017/BikeShop?authSource=admin` :
+    `mongodb+srv://${process.env.ATLAS_USERNAME}:${process.env.ATLAS_PASS}@bikeshop.v1dhfuf.mongodb.net/BikeShop?retryWrites=true&w=majority`,
 };
 
 export default db;

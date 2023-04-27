92691edaecf0b2f48d8539ad6b3306382223ef34
JoelWeers <42760707+JoelWeers@users.noreply.github.com>
2023-04-27 16:01:04 -0600

    added more Cart options and styles.

--- frontend/src/pages/Shop/cart.tsx
+++ frontend/src/pages/Shop/cart.tsx
@@ -18,15 +18,19 @@ import { AuthContext } from '@/context/auth.context';
 import { colors } from '@/styles/theme/Colors';
 import { DrawerHeaderProps } from '@react-navigation/drawer';
 
-
 const Cart = (props: DrawerHeaderProps) => {
+  //these prices are based off of https://www.ups.com/assets/resources/webcontent/en_US/daily_rates.pdf
+  const streetBikeShipping = 16.10;
+  const mountainBikeShipping = 19.45;
+  const eBikeShipping = 38.99;
+  const accessoriesShipping = 11.67;
 
   const { authUser, isLoggedIn } = useContext(AuthContext);
   const { products, quantity, total, checkout, removeFromCart, message } = useContext(ShopContext);
 
   const dimensions = useWindowDimensions();
   const checkMobile = (dimensions: ScaledSize) => { return (Platform.OS === 'android' || Platform.OS === 'ios' || dimensions.width <= 1450) ? true : false }
-  const styles = checkMobile(dimensions) ? mobile : web;
+  const responsive = checkMobile(dimensions) ? mobile : web;
 
   const [order, setOrder] = useState<Order | null>(null);
 
@@ -35,33 +39,75 @@ const Cart = (props: DrawerHeaderProps) => {
     setOrder(order);
   };
 
+  const calculateShipping = (products: Product[]) => {
+    let cost: number;
+
+    products.map(product => {
+      if (product.category == 'Bikes') {
+        if (product.subcategory == 'Mountain') {
+          cost += mountainBikeShipping;
+        } else if (product.subcategory == 'Street') {
+          cost += streetBikeShipping;
+        } else if (product.subcategory == 'Electric') {
+          cost += eBikeShipping;
+        }
+      }
+      else if (product.category == 'Accessories') {
+        cost += accessoriesShipping;
+      }
+      else if (product.category == 'Services') {
+        cost += 0;
+      }
+    });
+    return cost;
+  }
+
   return (
-    <ScrollView >
-      <View style={styles.container}>
-        <View style={styles.cart}>
-          <Text style={Layout.title}>Cart</Text>
-          <Text>authUser: {JSON.stringify(authUser)}</Text>
-
-
+    <View style={[Styles.container, responsive.container]}>
+      <View style={[Styles.cart, responsive.cart]}>
+        <Text style={[Styles.bodyText, { fontWeight: 'bold' }]}>Are your ready to checkout?  {isLoggedIn ? authUser.name + '?' : <Text />}</Text>
+        <Text style={Styles.bodyText}>Sub Total: {formatPrice(total)}</Text>
+        <Text style={Styles.bodyText}>Shipping: {products && formatPrice(calculateShipping(products))}</Text>
+        <Text style={Styles.bodyText}>Total:{formatPrice(total * 1.06)}</Text>
+        <Text style={Styles.bodyText}>Quantity: {quantity}</Text>
+        {
+          isLoggedIn
+            ?
+            <Text style={[Styles.bodyText, { fontWeight: 'bold' }]}>Proceed to Checkout</Text>
+            :
+            <Text style={[Styles.bodyText, { fontWeight: 'bold' }]} onPress={() => props.navigation.navigate("Login")}>Please Login to checkout your items</Text>
+        }
+        <TouchableOpacity
+          style={[Styles.button, Styles.checkoutBtn]}
+          onPress={isLoggedIn ? () => console.log("Checkout") : () => props.navigation.navigate("Login")}
+        >
+          <Text style={Styles.buttonText}>
+            Checkout <Icon size={20} name="checkbox-outline" />
+          </Text>
+        </TouchableOpacity>
+      </View>
+      <ScrollView >
+        <View style={Styles.cartScroll}>
+          <Text style={Styles.header}>Cart</Text>
           {products &&
             products.map((product: Product, index: number) => (
               <View style={{ margin: 10 }} key={index}>
-                <Text>{index}.</Text>
-                <Text>_id: {product?._id}</Text>
-                <Text>name: {product?.name}</Text>
-                <Text>description: {product?.description}</Text>
-                <Text>category: {product?.category}</Text>
-                <Text>subcategory: {product?.subcategory}</Text>
-                <Text>price: {product?.price}</Text>
-                <Text>imageIds: {product?.imageIds}</Text>
-                <Text>inStock: {product?.inStock}</Text>
+                <Text style={Styles.bodyText}>{index + 1}.</Text>
+                <Text style={Styles.bodyText}>_id: {product?._id}</Text>
+                <Text style={Styles.bodyText}>name: {product?.name}</Text>
+                <Text style={Styles.bodyText}>description: {product?.description}</Text>
+                <Text style={Styles.bodyText}>category: {product?.category}</Text>
+                <Text style={Styles.bodyText}>subcategory: {product?.subcategory}</Text>
+                <Text style={Styles.bodyText}>price: {product?.price}</Text>
+                <Text style={Styles.bodyText}>imageIds: {product?.imageIds}</Text>
+                <Text style={Styles.bodyText}>inStock: {product?.inStock}</Text>
                 {product.category === 'Bikes' && (
                   <>
-                    <Text>brand: {product?.brand}</Text>
-                    <Text>material: {product?.material}</Text>
-                    <Text>wheelSize: {product?.wheelSize}</Text>
-                    <Text>color: {product?.color}</Text>
-                    <Text>size: {product?.size}</Text>
+                    <Text style={Styles.bodyText}>brand: {product?.brand}</Text>
+                    <Text style={Styles.bodyText}>material: {product?.material}</Text>
+                    <Text style={Styles.bodyText}>wheelSize: {product?.wheelSize}</Text>
+                    <Text style={Styles.bodyText}>color: {product?.color}</Text>
+                    <Text style={Styles.bodyText}>size: {product?.size}</Text>
                   </>
                 )}
                 <TouchableOpacity
@@ -69,17 +115,16 @@ const Cart = (props: DrawerHeaderProps) => {
                   onPress={() => removeFromCart(product)}
                 >
                   <Text style={Styles.buttonText}>
-                    Remove <Icon size={15} name="trash-outline" />
+                    Remove From Cart <Icon size={15} name="trash-outline" />
                   </Text>
                 </TouchableOpacity>
               </View>
             ))}
         </View>
-        <View style={styles.cart}>
+        <View style={Styles.cart}>
           <Text>Sub Total: {formatPrice(total)}</Text>
           <Text>Total:</Text>
           <Text>Quantity: {quantity}</Text>
-          <Text>OrderItems: {JSON.stringify(items)}</Text>
           {isLoggedIn ? <Text>Proceed to Checkout</Text> : <Text onPress={() => props.navigation.navigate("Login")}>Please Login to checkout your items</Text>}
           <TouchableOpacity
             style={[Styles.button]}
@@ -104,8 +149,8 @@ const Cart = (props: DrawerHeaderProps) => {
             </>
           )}
         </View>
-      </View>
-    </ScrollView>
+      </ScrollView>
+    </View>
   );
 };
 
@@ -113,61 +158,75 @@ export default Cart;
 
 const mobile = StyleSheet.create({
   container: {
-    flex: 1,
     flexDirection: "column",
   },
   cart: {
-    justifyContent: "center",
-    borderWidth: 1,
-    backgroundColor: 'white',
-    borderColor: '#6A7B76',
-    borderRadius: 10,
+    marginTop: 90,
   },
 });
 
 const web = StyleSheet.create({
   container: {
-    flex: 1,
     flexDirection: "row",
   },
   cart: {
+
+  }
+});
+
+const Styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    // flexDirection: "row",
+  },
+  cart: {
+    flex: 1,
+    flexDirection: "column",
     justifyContent: "center",
+
     borderWidth: 1,
     backgroundColor: 'white',
     borderColor: '#6A7B76',
     borderRadius: 10,
   },
-});
-
-const Styles = StyleSheet.create({
-  button: {
+  cartScroll: {
     flex: 1,
+    flexDirection: "column",
+  },
+  button: {
+    // flex: 1,
+    // flexDirection: "row",
+    height: 50,
+    width: 150,
     padding: 10,
     margin: 3,
     borderRadius: 10,
-    textAlign: 'center',
+    // textAlign: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
     color: 'white',
   },
   buttonText: {
     color: 'white',
     fontWeight: 'bold',
+    justifyContent: 'center',
   },
   deleteBtn: {
     backgroundColor: '#941b0c',
   },
   checkoutBtn: {
     backgroundColor: colors.artichoke,
-    header: {
-      marginTop: 10,
-      fontSize: checkMobile(dimensions) ? 45 : 25,
-      color: "#262626",
-      textAlign: 'center',
-    },
-    bodyText: {
-      marginTop: 15,
-      fontSize: 24,
-      color: "#262626",
-      textAlign: "center",
-    },
-  });
+  },
+  header: {
+    marginTop: 10,
+    fontSize: 35,
+    color: "#262626",
+    textAlign: 'center',
+  },
+  bodyText: {
+    marginTop: 15,
+    fontSize: 24,
+    color: "#262626",
+    textAlign: "center",
+  },
+});

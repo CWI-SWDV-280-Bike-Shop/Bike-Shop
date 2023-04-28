16b67c4138eb16502195f6269928fdd0eb9dbb11
William Beukelman <williambeukelman@my.cwi.edu>
2023-04-28 01:55:49 -0600

    experimental home page styling and images

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import Icon from 'react-native-vector-icons/Ionicons';
-import { Text, StyleSheet, View, ScrollView, Platform, ScaledSize } from 'react-native';
+import { Text, StyleSheet, View, ScrollView, Platform, ScaledSize, Image, TouchableOpacity } from 'react-native';
 import Swiper from 'react-native-web-swiper';
 // Slide component imports
 import { Slide } from '@components/Slide';
@@ -28,6 +28,7 @@ const CustomSwiper = ({dimensions} : {dimensions : ScaledSize}) => {
     loop
     timeout={5}
     springConfig={{ speed: 5, bounciness: 1, }}
+    containerStyle={web.slides}
     controlsProps={{
       prevTitle: '⮜',
       nextTitle: '⮞',
@@ -96,6 +97,23 @@ const mission = [`Our mission at Wheely Good Bikes is to provide high-quality bi
 export const Home = ({dimensions} : {dimensions : ScaledSize}) => {
   const styles = (checkMobile(dimensions)) ? mobile : web;
   const slideNames = ['City Bikes'];
+  const content = [
+    {
+      "image": require('../assets/Images/home_bikes.jpg'),
+      "hero": "Bicycles",
+      "content": "Our mission is to provide high-quality bicycles and accessories that promote a healthy and sustainable lifestyle. We are committed to offering personalized service and expert advice to help our customers choose the right bike for their needs and skill level."
+    },
+    {
+      "image": require('../assets/Images/home_repair.jpg'),
+      "hero": "Services",
+      "content": "Our goal is to create a welcoming environment where cyclists of all ages and abilities can come together to share their passion for cycling and enjoy the freedom of the open road."
+    },
+    {
+      "image": require('../assets/Images/home_accessories.jpg'),
+      "hero": "Accessories",
+      "content": "At the heart of our mission is a dedication to promoting eco-friendly transportation options and helping our community reduce its carbon footprint."
+    }
+  ]
   return (checkMobile(dimensions)) ?
     (
       <ScrollView>
@@ -120,43 +138,119 @@ export const Home = ({dimensions} : {dimensions : ScaledSize}) => {
     ) : (
     <ScrollView>
       <View style={[styles.container]}>
-      <View style={[styles.slidesContainer]}>
-        <CustomSwiper dimensions={dimensions}/>
-      </View>
-      <View style={[styles.contentContainer]}>
-          <View style={[styles.quoteRow]}>
-            <Text style={[styles.quote]}>
-              {quote}
-            </Text>
-          </View>
-          <View style={[styles.missionStatementRow]}>
-            <Icon name="megaphone-outline"
-              size={40}
-            />
-            <Text style={[styles.missionStatement]}>
-              {mission[0]}
-            </Text>
-            <Icon name="flag-outline"
-              size={40}
-            />
-            <Text style={[styles.missionStatement]}>
-              {mission[1]}
-            </Text>
-            <Icon name="heart-outline"
-              size={40}
-            />
-            <Text style={[styles.missionStatement]}>
-              {mission[2]}
-            </Text>
-          </View>
-      </View>
-      <Footer />
+        <View style={[styles.slidesContainer]}>
+          <CustomSwiper dimensions={dimensions}/>
+        </View>
+        <View style={[styles.contentContainer]}>
+          {
+            content.map((item, i) => (
+              <View style={[styles.row, (i%2==0) ? styles.evenRow : styles.oddRow]} key={i}>
+                <View style={styles.bodyimage}>
+                <Image
+                  source={item.image}
+                  style={styles.bodyimageBox}
+                />
+                </View>
+                <View style={styles.block}>
+                  <Text style={styles.heroText}>{item.hero}</Text>
+                  <Text style={styles.basicText}>{item.content}</Text>
+                  <View style={styles.buttonGroup}>
+                    <TouchableOpacity style={styles.button}>
+                      <Text style={styles.buttonText}>View {item.hero}</Text>
+                    </TouchableOpacity>
+                  </View>
+                </View>
+              </View>
+            ))
+          }
+            {/* <View style={[styles.quoteRow]}>
+              <Text style={[styles.quote]}>
+                {quote}
+              </Text>
+            </View>
+            <View style={[styles.missionStatementRow]}>
+              <Icon name="megaphone-outline"
+                size={40}
+              />
+              <Text style={[styles.missionStatement]}>
+                {mission[0]}
+              </Text>
+              <Icon name="flag-outline"
+                size={40}
+              />
+              <Text style={[styles.missionStatement]}>
+                {mission[1]}
+              </Text>
+              <Icon name="heart-outline"
+                size={40}
+              />
+              <Text style={[styles.missionStatement]}>
+                {mission[2]}
+              </Text>
+            </View> */}
+        </View>
+        <Footer />
     </View>
   </ScrollView>
     );
   };
 
 const mobile = StyleSheet.create({
+  button: {
+    paddingVertical: 10,
+    paddingHorizontal: 30,
+    backgroundColor: 'transparent',
+    borderColor: '#3fab82',
+    borderWidth: 1,
+    borderRadius: 10,
+  },
+  buttonGroup: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1
+  },
+  buttonText: {
+    color: '#3fab82',
+    fontWeight: 'bold',
+    fontSize: 16,
+    textTransform: 'uppercase'
+  },
+  row: {
+    flexDirection: 'row',
+    paddingHorizontal: 30,
+    margin: 40,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    columnGap: 20,
+  },
+  evenRow: {
+      flexDirection: 'row'
+  },
+  oddRow: {
+      flexDirection: 'row-reverse'
+  },
+  bodyimage: {
+    height: 400,
+    width: 400,
+    backgroundColor: '#477b61'
+  },
+  bodyimageBox: {
+    height: 400,
+    width: 400,
+    resizeMode: 'cover',
+    opacity: 0.25
+  },
+  heroText: {
+      textTransform: 'uppercase',
+      fontSize: 60,
+      fontWeight: 'bold'
+  },
+  block: {
+      flexDirection: 'column'
+  },
+  basicText: {
+      fontSize: 18,
+  },
   container: {
     flex: 1,
     flexDirection: 'column',
@@ -219,20 +313,97 @@ const mobile = StyleSheet.create({
 
 //Web Stylesheet
 const web = StyleSheet.create({
+  button: {
+    paddingVertical: 10,
+    paddingHorizontal: 30,
+    backgroundColor: 'transparent',
+    borderColor: '#3fab82',
+    borderWidth: 1,
+    borderRadius: 5,
+  },
+  buttonGroup: {
+    marginTop: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1
+  },
+  buttonText: {
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 18,
+    textTransform: 'uppercase'
+  },
+  slides: {
+    height: 600,
+  },
+  row: {
+    flexDirection: 'row',
+    paddingHorizontal: 30,
+    marginHorizontal: 30,
+    marginBottom: -30,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    columnGap: 20,
+  },
+  evenRow: {
+      flexDirection: 'row',
+  },
+  oddRow: {
+      flexDirection: 'row-reverse',
+  },
+  bodyimage: {
+    borderRadius: 200,
+    backgroundColor: '#3fab82'
+  },
+  bodyimageBox: {
+    borderRadius: 200,
+    height: 400,
+    width: 400,
+    opacity: .65,
+    resizeMode: 'cover',
+  },
+  heroText: {
+    textTransform: 'uppercase',
+    fontSize: 60,
+    fontWeight: 'bold',
+    color: '#fff'
+  },
+  block: {
+      borderColor: '#3fab82',
+      borderTopWidth: 10,
+      flexDirection: 'column',
+      padding: 30,
+      paddingHorizontal: 50,
+      shadowColor: "#000",
+      shadowOffset: {
+        width: 0,
+        height: 1,
+      },
+      shadowOpacity: 0.22,
+      shadowRadius: 2.22,
+      elevation: 1,
+      flexWrap: 'wrap',
+      maxWidth: 600,
+      backgroundColor: '#ffffff20'
+  },
+  basicText: {
+      fontSize: 18,
+      color: '#fff'
+  },
   container: {
     flex: 1,
     flexDirection: 'column',
   },
   slidesContainer: {
     flexDirection: 'row',
-    flex: 3,
   },
   contentContainer: {
     flexDirection: 'column',
     flex: 2,
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingHorizontal: '10%'
+    paddingHorizontal: '5%',
+    paddingVertical: 40,
+    paddingBottom: 70,
+    backgroundColor: '#1F302D',
   },
   quoteRow: {
     margin: 10,
@@ -257,19 +428,16 @@ const web = StyleSheet.create({
     backgroundColor: '#0000002a',
     padding: 15,
     borderRadius: 10,
-    //flex: 1, 
     marginTop: 20,
     marginBottom: 20,
   },
   missionStatement: {
-    /* borderRightWidth: 5,
-    borderColor: '#03312E', */
     alignSelf: 'flex-start',
     textAlign: 'left',
     fontSize: 20,
     color: '#03312E',
     paddingLeft: 15,
     paddingRight: 15
-  },
+  }
 });
 

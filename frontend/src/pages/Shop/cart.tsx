912f41be31b40d90a48f07bcc0d23a204679e7de
JoelWeers <42760707+JoelWeers@users.noreply.github.com>
2023-04-28 11:48:13 -0600

    Fixed up the styling more and added some better formating.

--- frontend/src/pages/Shop/cart.tsx
+++ frontend/src/pages/Shop/cart.tsx
@@ -18,6 +18,8 @@ import { AuthContext } from '@/context/auth.context';
 import { colors } from '@/styles/theme/Colors';
 import { DrawerHeaderProps } from '@react-navigation/drawer';
 
+
+
 const Cart = (props: DrawerHeaderProps) => {
   //these prices are based off of https://www.ups.com/assets/resources/webcontent/en_US/daily_rates.pdf
   const streetBikeShipping = 16.10;
@@ -40,7 +42,7 @@ const Cart = (props: DrawerHeaderProps) => {
   };
 
   const calculateShipping = (products: Product[]) => {
-    let cost: number;
+    let cost = 0;
 
     products.map(product => {
       if (product.category == 'Bikes') {
@@ -62,14 +64,28 @@ const Cart = (props: DrawerHeaderProps) => {
     return cost;
   }
 
+  // const ListOrderItems = (items: OrderItem[]) => {
+  //   let orderItems = "\n";
+  //   items.map((item: OrderItem) => {
+  //     orderItems += "Product: " + item.product + " "
+  //     orderItems += "Item Price: " + formatPrice(item.price) + " "
+  //     orderItems += '\n'
+  //   });
+  //   return orderItems;
+  // }
+
   return (
     <View style={[Styles.container, responsive.container]}>
       <View style={[Styles.cart, responsive.cart]}>
-        <Text style={[Styles.bodyText, { fontWeight: 'bold' }]}>Are your ready to checkout?  {isLoggedIn ? authUser.name + '?' : <Text />}</Text>
+        <Text style={[Styles.bodyText, { fontWeight: 'bold', fontSize: 30 }]}>Are your ready to checkout?  {isLoggedIn ? authUser.name + '?' : <Text />}</Text>
+        <Text style={Styles.bodyText}>Quantity of Items/Services: {quantity}</Text>
+        {/* <Text style={Styles.bodyText}>OrderItems: {items && ListOrderItems(items)}</Text> */}
         <Text style={Styles.bodyText}>Sub Total: {formatPrice(total)}</Text>
         <Text style={Styles.bodyText}>Shipping: {products && formatPrice(calculateShipping(products))}</Text>
         <Text style={Styles.bodyText}>Total:{formatPrice(total * 1.06)}</Text>
         <Text style={Styles.bodyText}>Quantity: {quantity}</Text>
+        <Text style={Styles.bodyText}>State Tax: {formatPrice(total * 0.06)}</Text>
+        <Text style={Styles.bodyText}>Grand Total:{formatPrice((total * 1.06) + (products && calculateShipping(products)))}</Text>
         {
           isLoggedIn
             ?
@@ -79,35 +95,35 @@ const Cart = (props: DrawerHeaderProps) => {
         }
         <TouchableOpacity
           style={[Styles.button, Styles.checkoutBtn]}
-          onPress={isLoggedIn ? () => console.log("Checkout") : () => props.navigation.navigate("Login")}
+          onPress={isLoggedIn ? () => handleCheckout : () => props.navigation.navigate("Login")}
         >
           <Text style={Styles.buttonText}>
             Checkout <Icon size={20} name="checkbox-outline" />
           </Text>
         </TouchableOpacity>
       </View>
-      <ScrollView >
-        <View style={Styles.cartScroll}>
+      <View style={Styles.cartScroll}>
+        <ScrollView >
           <Text style={Styles.header}>Cart</Text>
           {products &&
             products.map((product: Product, index: number) => (
-              <View style={{ margin: 10 }} key={index}>
-                <Text style={Styles.bodyText}>{index + 1}.</Text>
-                <Text style={Styles.bodyText}>_id: {product?._id}</Text>
-                <Text style={Styles.bodyText}>name: {product?.name}</Text>
-                <Text style={Styles.bodyText}>description: {product?.description}</Text>
-                <Text style={Styles.bodyText}>category: {product?.category}</Text>
-                <Text style={Styles.bodyText}>subcategory: {product?.subcategory}</Text>
-                <Text style={Styles.bodyText}>price: {product?.price}</Text>
-                <Text style={Styles.bodyText}>imageIds: {product?.imageIds}</Text>
-                <Text style={Styles.bodyText}>inStock: {product?.inStock}</Text>
+              <View style={Styles.productCard} key={index}>
+                <Text style={[Styles.bodyText, Styles.cardText]}>{index + 1}.</Text>
+                <Text style={[Styles.bodyText, Styles.cardText]}>name: {product?.name}</Text>
+                <Text style={[Styles.bodyText, Styles.cardText]}>_id: {product?._id}</Text>
+                <Text style={[Styles.bodyText, Styles.cardText]}>description: {product?.description}</Text>
+                <Text style={[Styles.bodyText, Styles.cardText]}>category: {product?.category}</Text>
+                <Text style={[Styles.bodyText, Styles.cardText]}>subcategory: {product?.subcategory}</Text>
+                <Text style={[Styles.bodyText, Styles.cardText]}>price: {product?.price}</Text>
+                <Text style={[Styles.bodyText, Styles.cardText]}>imageIds: {product?.imageIds}</Text>
+                <Text style={[Styles.bodyText, Styles.cardText]}>inStock: {product?.inStock}</Text>
                 {product.category === 'Bikes' && (
                   <>
-                    <Text style={Styles.bodyText}>brand: {product?.brand}</Text>
-                    <Text style={Styles.bodyText}>material: {product?.material}</Text>
-                    <Text style={Styles.bodyText}>wheelSize: {product?.wheelSize}</Text>
-                    <Text style={Styles.bodyText}>color: {product?.color}</Text>
-                    <Text style={Styles.bodyText}>size: {product?.size}</Text>
+                    <Text style={[Styles.bodyText, Styles.cardText]}>brand: {product?.brand}</Text>
+                    <Text style={[Styles.bodyText, Styles.cardText]}>material: {product?.material}</Text>
+                    <Text style={[Styles.bodyText, Styles.cardText]}>wheelSize: {product?.wheelSize}</Text>
+                    <Text style={[Styles.bodyText, Styles.cardText]}>color: {product?.color}</Text>
+                    <Text style={[Styles.bodyText, Styles.cardText]}>size: {product?.size}</Text>
                   </>
                 )}
                 <TouchableOpacity
@@ -120,37 +136,38 @@ const Cart = (props: DrawerHeaderProps) => {
                 </TouchableOpacity>
               </View>
             ))}
-        </View>
-        <View style={Styles.cart}>
-          <Text>Sub Total: {formatPrice(total)}</Text>
-          <Text>Total:</Text>
-          <Text>Quantity: {quantity}</Text>
-          {isLoggedIn ? <Text>Proceed to Checkout</Text> : <Text onPress={() => props.navigation.navigate("Login")}>Please Login to checkout your items</Text>}
-          <TouchableOpacity
-            style={[Styles.button]}
-            onPress={isLoggedIn ? () => console.log("Checkout") : () => { return; }}
-          >
-            <Text style={Styles.buttonText}>
-              Checkout<Icon size={15} name="checkbox-outline" />
-            </Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={[Styles.button, Styles.checkoutBtn]}
-            onPress={handleCheckout}
-          >
-            <Text style={Styles.buttonText}>
-              Checkout <Icon size={15} name="cart-outline" />
-            </Text>
-          </TouchableOpacity>
-          {order && (
-            <>
-              <Text>{message}</Text>
-              <Text>order: {JSON.stringify(order)}</Text>
-            </>
-          )}
-        </View>
-      </ScrollView>
-    </View>
+        </ScrollView>
+
+      </View>
+      {/* <View style={Styles.cart}>
+        <Text>Sub Total: {formatPrice(total)}</Text>
+        <Text>Total:</Text>
+        <Text>Quantity: {quantity}</Text>
+        {isLoggedIn ? <Text>Proceed to Checkout</Text> : <Text onPress={() => props.navigation.navigate("Login")}>Please Login to checkout your items</Text>}
+        <TouchableOpacity
+          style={[Styles.button]}
+          onPress={isLoggedIn ? () => console.log("Checkout") : () => { return; }}
+        >
+          <Text style={Styles.buttonText}>
+            Checkout<Icon size={15} name="checkbox-outline" />
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={[Styles.button, Styles.checkoutBtn]}
+          onPress={handleCheckout}
+        >
+          <Text style={Styles.buttonText}>
+            Checkout <Icon size={15} name="cart-outline" />
+          </Text>
+        </TouchableOpacity>
+        {order && (
+          <>
+            <Text>{message}</Text>
+            <Text>order: {JSON.stringify(order)}</Text>
+          </>
+        )}
+    </View> */}
+    </View >
   );
 };
 
@@ -180,9 +197,10 @@ const Styles = StyleSheet.create({
     // flexDirection: "row",
   },
   cart: {
-    flex: 1,
+    flex: 1.5,
     flexDirection: "column",
     justifyContent: "center",
+    margin: 5,
 
     borderWidth: 1,
     backgroundColor: 'white',
@@ -190,8 +208,19 @@ const Styles = StyleSheet.create({
     borderRadius: 10,
   },
   cartScroll: {
-    flex: 1,
+    margin: 5,
+
+    flex: 4,
     flexDirection: "column",
+    justifyContent: 'center',
+  },
+  productCard: {
+    margin: 10,
+
+    borderWidth: 1,
+    backgroundColor: 'white',
+    borderColor: '#6A7B76',
+    borderRadius: 10,
   },
   button: {
     // flex: 1,
@@ -228,5 +257,9 @@ const Styles = StyleSheet.create({
     fontSize: 24,
     color: "#262626",
     textAlign: "center",
+  },
+  cardText: {
+    margin: 4,
+    fontSize: 15,
   },
 });

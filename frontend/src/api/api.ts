f6278d6cce6185b37253f6e4eb201a035f36a6e0
William Beukelman <williambeukelman@my.cwi.edu>
2023-04-28 09:10:32 -0600

    removed prefix

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -3,10 +3,9 @@ import TokenAPI from './token.api';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const url = __DEV__ ? "localhost:8081" : "thebikeshop.app"
-const prefix = __DEV__ ? "http" : "https"
 
 const api = axios.create({
-  baseURL: `${prefix}://${url}/api`,
+  baseURL: `$//${url}/api`,
   headers: {
     'Content-type': 'application/json',
   },

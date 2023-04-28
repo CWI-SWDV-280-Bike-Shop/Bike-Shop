f82687eba040912f033ab7f65a5b834dd9f88995
William Beukelman <williambeukelman@my.cwi.edu>
2023-04-27 22:45:46 -0600

    Added popover menu for profile options on desktop

--- frontend/src/components/Navigation/Header.tsx
+++ frontend/src/components/Navigation/Header.tsx
@@ -1,5 +1,5 @@
 import { DrawerHeaderProps } from '@react-navigation/drawer';
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import {
   TouchableOpacity,
   Text,
@@ -15,6 +15,7 @@ import Icon from 'react-native-vector-icons/Ionicons';
 import { AuthContext } from '@context/auth.context';
 import { Pressable } from 'react-native-web-hover';
 import { ShopContext } from '@/context/shop.context';
+import Popover from "react-native-popover-view";
 
 const HamburgerMenu = ({ navigation }: DrawerHeaderProps) => {
   return (
@@ -65,7 +66,7 @@ export const NavigationHeader = (props: DrawerHeaderProps) => {
     return (
       <View style={styles.navBar}>
         {navigation.getState().routeNames.map((name, i) => {
-          if (name === 'Cart') {
+          if (name === 'Cart' || name === 'Profile') {
             return;
           }
           if (
@@ -88,7 +89,7 @@ export const NavigationHeader = (props: DrawerHeaderProps) => {
   };
 
   const currentRoute = nav.getState().routeNames[nav.getState().index];
-
+  const [showPopover, setShowPopover] = useState(false);
   return (
     <View
       style={[
@@ -105,6 +106,19 @@ export const NavigationHeader = (props: DrawerHeaderProps) => {
         />
       </View>
       <View style={[styles.headerIcons, { justifyContent: 'flex-end' }]}>
+      <Popover
+          popoverStyle={{ backgroundColor: '#ffffff00' }}
+          backgroundStyle={{ backgroundColor: "transparent" }}
+          from={
+            <TouchableOpacity 
+            style={[styles.headerTouchable, (checkMobile(dimensions) ? {display: 'none'} : {})]}
+            onPress={() => setShowPopover(!showPopover)}>
+              <ProfileButton />
+            </TouchableOpacity>
+          }
+        >
+          <ProfilePopup navigation={props.navigation}/>
+        </Popover>
         <TouchableOpacity
           style={styles.headerTouchable}
           onPress={() => props.navigation.navigate('Cart')}
@@ -112,6 +126,67 @@ export const NavigationHeader = (props: DrawerHeaderProps) => {
           <CartButton />
         </TouchableOpacity>
       </View>
+    </View>
+  );
+};
+
+const ProfilePopup = ({navigation}) => {
+  const { isLoggedIn, authUser } = useContext(AuthContext);
+  const username = isLoggedIn && authUser.name;
+  const email = isLoggedIn && authUser.email;
+
+  return (
+    <View style={styles.popoverBody}>
+      <View style={styles.profileRow}>
+        <View style={styles.profileImage}><Icon name="person-circle-outline" size={100} color="#FFF" /></View>
+        <View style={styles.profileDetails}>
+          <Text style={styles.profileName}>{username}</Text>
+          <Text style={styles.profileEmail}>{email}</Text>
+          <TouchableOpacity
+          style={styles.buttonPrimary}
+          onPress={() => navigation.navigate('Profile')}
+        >
+          <Text style={styles.btnFont}>Edit Profile</Text>
+        </TouchableOpacity>
+        </View>
+      </View>
+      <View style={styles.profileLinks}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => navigation.navigate('Profile')}
+        >
+          <View style={styles.iconlabelGrouping}>
+            <Icon name="receipt-outline" size={24} color="#333333ee" />
+            <Text style={styles.buttonContent}>Orders</Text>
+          </View>
+          <Icon name="chevron-forward-outline" size={24} color="#333333ee"/>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => navigation.navigate('Profile')}
+        >
+          <View style={styles.iconlabelGrouping}>
+            <Icon name="key-outline" size={24} color="#333333ee" />
+            <Text style={styles.buttonContent}>Admin</Text>
+          </View>
+          <Icon name="chevron-forward-outline" size={24} color="#333333ee"/>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button}>
+          <View style={styles.iconlabelGrouping}>
+            <Icon name="log-out-outline" size={24} color="#333333ee" />
+            <Text style={styles.buttonContent}>Logout</Text>
+          </View>
+          <Icon name="chevron-forward-outline" size={24} color="#333333ee"/>
+        </TouchableOpacity>
+      </View>
+    </View>
+  )
+}
+
+const ProfileButton = () => {
+  return (
+    <View style={styles.cartContainer}>
+      <Icon name="person-outline" size={40} color="#FFF" />
     </View>
   );
 };
@@ -132,6 +207,89 @@ const CartButton = () => {
 
 //Header Stylesheet
 const styles = StyleSheet.create({
+  /* Popover Profile Stuff */
+  popoverBody: {
+    backgroundColor: '#ffffffdd',
+    borderRadius: 25,
+    borderColor: '#477B6133',
+    borderWidth: 5,
+    padding: 10,
+    paddingVertical: 20,
+  },
+  popoverText: {
+    color: '#fff'
+  },
+  profileImage: {
+    width: 150,
+    height: 150,
+    borderRadius: 20,
+    backgroundColor: '#444',
+    margin: 10,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  profileRow: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingVertical: 10,
+    paddingHorizontal: 30,
+  },
+  profileLinks: {
+    paddingVertical: 20,
+    marginHorizontal: 40,
+    flexDirection: 'column',
+    columnGap: 15,
+    borderTopWidth: 1,
+    borderBottomWidth: 1,
+    borderColor: '#33333342'
+  },
+  profileDetails: {
+    gap: 15,
+    marginHorizontal: 20,
+    flexDirection: 'column',
+    alignItems: 'flex-start',
+  },
+  profileName: {
+    fontWeight: 'bold',
+    fontSize: 22,
+  },
+  profileEmail: {
+    fontSize: 18,
+  },
+  iconlabelGrouping: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 20,
+  },
+  buttonPrimary: {
+    backgroundColor: '#477B61',
+    paddingHorizontal: 30,
+    paddingVertical: 10,
+    alignContent: 'center',
+    justifyContent: 'center',
+    borderRadius: 5,
+  },
+  btnFont: {
+    fontWeight: 'bold',
+    color: '#fff',
+  },
+  button: {
+    marginVertical: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    gap: 20,
+    backgroundColor: '#03312E00',
+    padding: 10,
+    borderRadius: 10,
+    alignItems: 'center',
+  },
+  buttonContent: {
+    color: '#333333ee',
+    textAlign: 'center',
+    fontSize: 20,
+  },
+  /* Other Styles */
   active: {
     borderBottomColor: '#fff',
     borderBottomWidth: 5,

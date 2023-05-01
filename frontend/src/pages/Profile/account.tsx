3098327368e7a60f6c01b0a446f1c8ad67053bee
Nicko Goodwin <33714480+nickogoodwin@users.noreply.github.com>
2023-05-01 02:42:02 -0600

    Update account.tsx
    the app fails if a user were to logout while on the account page, or navigate to the account page while not logged in. Went ahead and fixed it ;)

--- frontend/src/pages/Profile/account.tsx
+++ frontend/src/pages/Profile/account.tsx
@@ -1,76 +1,87 @@
-import React, {useState} from 'react';
-import {  Text, StyleSheet, View, TouchableOpacity, TextInput } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import {
+  Text,
+  StyleSheet,
+  View,
+  TouchableOpacity,
+  TextInput,
+} from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 import { AuthContext } from '@context/auth.context';
 import UserAPI from '@api/user.api';
 import { useContext } from 'react';
 import { ScrollView } from 'react-native-gesture-handler';
-import { Product, Order, User, OrderItem } from "@/types/data.types";
+import { Product, Order, User, OrderItem } from '@/types/data.types';
 import { DrawerHeaderProps } from '@react-navigation/drawer';
 
 export const Account = (props: DrawerHeaderProps) => {
+  const [modalVisible, setModalVisible] = useState(false);
+  const [editType, setEditType] = useState('');
+  const { isLoggedIn, authUser } = useContext(AuthContext);
+  const username = isLoggedIn && authUser.name;
+  const email = isLoggedIn && authUser.email;
+  const password = isLoggedIn && authUser.password;
+  const [user, setUser] = useState({} as User);
 
-    const [modalVisible, setModalVisible] = useState(false);
-    const [editType, setEditType] = useState("");
-    const { isLoggedIn, authUser } = useContext(AuthContext);
-    const username = isLoggedIn && authUser.name;
-    const email = isLoggedIn && authUser.email;
-    const password = isLoggedIn && authUser.password;
-    const [user, setUser] = useState({} as User);
-
-    if(authUser == undefined){
-      props.navigation.navigate("Login")
+  useEffect(() => {
+    if (!authUser) {
+      props.navigation.navigate('Login');
     }
+  }, [authUser]);
 
-    UserAPI.getById(authUser._id).then((res) => setUser(res.data));
+  if (authUser) UserAPI.getById(authUser._id).then((res) => setUser(res.data));
 
-    const [form, setForm] = useState({
-      "username" : username,
-      "email" : email,
-      "password" : password,
-      "phone": user.phone,
-      "street": user.address?.street,
-      "state": user.address?.state,
-      "zipcode": user.address?.zip,
-      "country": user.address?.country,
-      });
+  const [form, setForm] = useState({
+    username: username,
+    email: email,
+    password: password,
+    phone: user.phone,
+    street: user.address?.street,
+    state: user.address?.state,
+    zipcode: user.address?.zip,
+    country: user.address?.country,
+  });
 
-    const fields = [
-      {"name": "username", "label": "Username"},
-      {"name": "email", "label": "Email"},
-      {"name": "password", "label": "Password"},
-      {"name": "phone", "label": "Phone"},
-      {"name": "street", "label": "Address"},
-      {"name": "state", "label": "State"},
-      {"name": "zipcode", "label": "Zip Code"},
-      {"name": "country", "label": "Country"},
-    ]
+  const fields = [
+    { name: 'username', label: 'Username' },
+    { name: 'email', label: 'Email' },
+    { name: 'password', label: 'Password' },
+    { name: 'phone', label: 'Phone' },
+    { name: 'street', label: 'Address' },
+    { name: 'state', label: 'State' },
+    { name: 'zipcode', label: 'Zip Code' },
+    { name: 'country', label: 'Country' },
+  ];
 
-    return (
-      <View style={[styles.container]}>
-        <ScrollView>
+  return !authUser ? (
+    <View style={styles.container}>
+      <Text>You must be logged in to view this page.</Text>
+    </View>
+  ) : (
+    <View style={[styles.container]}>
+      <ScrollView>
         <View style={styles.profileDetails}>
-          <View style={styles.profileImage}><Icon name="person-circle-outline" size={100} color="#FFF" /></View>
+          <View style={styles.profileImage}>
+            <Icon name="person-circle-outline" size={100} color="#FFF" />
+          </View>
           <Text style={styles.profileName}>{username}</Text>
         </View>
         <View style={styles.accountDetails}>
-          {
-            fields.map((item, i) => (
-              <View style={styles.row} key={i}>
-                <Text style={styles.label}>{item.label}</Text>
-                <TextInput value={form[item.name]} style={styles.editBox} />
-              </View>
-            ))
-          }
+          {fields.map((item, i) => (
+            <View style={styles.row} key={i}>
+              <Text style={styles.label}>{item.label}</Text>
+              <TextInput value={form[item.name]} style={styles.editBox} />
+            </View>
+          ))}
           <View style={styles.rowBottom}>
-            <TouchableOpacity style={styles.buttonPrimary} >
+            <TouchableOpacity style={styles.buttonPrimary}>
               <Text style={styles.btnFont}>Save Changes</Text>
             </TouchableOpacity>
           </View>
         </View>
-        </ScrollView>
-      </View>
-    );
+      </ScrollView>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
@@ -81,7 +92,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#444',
     margin: 10,
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
   },
   accountDetails: {
     marginHorizontal: 40,
@@ -92,7 +103,7 @@ const styles = StyleSheet.create({
   },
   row: {
     justifyContent: 'space-around',
-    paddingHorizontal: 'auto'
+    paddingHorizontal: 'auto',
   },
   rowBottom: {
     flexDirection: 'row',
@@ -105,15 +116,15 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     position: 'relative',
     bottom: -30,
-    left: 20
-  }, 
+    left: 20,
+  },
   editBox: {
     paddingTop: 32,
     padding: 16,
     marginBottom: 20,
     margin: 5,
     borderRadius: 10,
-    shadowColor: "#000",
+    shadowColor: '#000',
     shadowOffset: {
       width: 0,
       height: 1,
@@ -127,7 +138,7 @@ const styles = StyleSheet.create({
     marginVertical: 20,
     flexDirection: 'column',
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
   },
   profileName: {
     fontWeight: 'bold',
@@ -147,16 +158,16 @@ const styles = StyleSheet.create({
     color: '#fff',
   },
   container: {
-    flex: 1
+    flex: 1,
   },
   contentContainer: {
     flex: 1,
     flexDirection: 'column',
     backgroundColor: '#D3D5D4',
-  }, 
+  },
   bodyText: {
     margin: 10,
     fontSize: 30,
-    color: "#262626",
+    color: '#262626',
   },
 });

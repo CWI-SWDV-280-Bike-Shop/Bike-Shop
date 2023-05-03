0526c86566ee4d8176e4356232f589fadddf066a
Nicko Goodwin <33714480+nickogoodwin@users.noreply.github.com>
2023-05-02 23:33:06 -0600

    added Address Schema & Types, updated Order model to include shippingAddress

--- frontend/src/types/data.types.ts
+++ frontend/src/types/data.types.ts
@@ -4,6 +4,14 @@ type Data = {
   updatedAt?: string;
 };
 
+export type Address = {
+  street?: string;
+  city?: string;
+  state?: string;
+  zip?: string;
+  country?: string;
+};
+
 // Auth
 export type Credentials = {
   email: string;
@@ -27,13 +35,7 @@ export type AuthUser = Data & {
 // User
 export type User = AuthUser & {
   phone?: string;
-  address?: {
-    street?: string;
-    city?: string;
-    state?: string;
-    zip?: string;
-    country?: string;
-  };
+  address?: Address;
   orders?: [Order];
 };
 
@@ -59,11 +61,11 @@ export type OrderItem = Data & {
   product: Product | string;
   price: number;
   quantity: number;
-  serviceDate?: string;
 };
 
 export type Order = Data & {
   customer?: User | string;
   items?: OrderItem[];
+  shippingAddress: Address;
   total?: number;
 };
